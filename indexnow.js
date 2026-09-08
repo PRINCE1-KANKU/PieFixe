0173f4840f85f0293b9d360d8ca894c31b7d5ed4
prof002 <78856980+PRINCE1-KANKU@users.noreply.github.com>
2026-09-08 08:17:21 +0100

    Create indexnow.js

---
+++ indexnow.js
@@ -0,0 +1,13 @@
+POST /IndexNow HTTP/1.1
+Content-Type: application/json; charset=utf-8
+Host: api.indexnow.org
+{
+  "host": "www.example.org",
+  "key": "1df39f74fbd54eb49111d6b15869542f",
+  "keyLocation": "https://www.piefixe.co.za/1df39f74fbd54eb49111d6b15869542f.txt",
+  "urlList": [
+      "https:/www.piefixe.co.za/index.html",
+      "https://www.piefixe.co.za/blog.html",
+      "https://www.piefixe.co.za/contact.html"
+      ]
+}
